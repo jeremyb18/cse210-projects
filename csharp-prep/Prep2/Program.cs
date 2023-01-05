603ec5739e4315a96ef6395525e8a897684b63d9
Jeremy Ruebush <jeremyb31415@gmail.com>
2023-01-05 13:06:40 -0700

    Finish Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string StringGrade = Console.ReadLine();
+        int NumberGrade = int.Parse(StringGrade);
+        string letter = "F";
+        if ( NumberGrade >= 90)
+        {
+            letter = "A";
+        } else if (NumberGrade >= 80){
+            letter = "B";
+        } else if (NumberGrade >= 70){
+            letter = "C";
+        } else if (NumberGrade >= 60){
+            letter = "D";
+        }
+        Console.WriteLine($"Your grade is a {letter}");
+        if (NumberGrade >= 70)
+        {
+            Console.WriteLine($"Congratulation you passed your class");
+        } else {
+            Console.WriteLine($"You didn't pass this time but we know you can do it just keep trying!");
+        }
+        
     }
 }

070fe0d5e58cac992d32e30b9d0366d8eed1258d
Jeremy Ruebush <jeremyb31415@gmail.com>
2023-01-09 09:53:01 -0700

    Finished Pres 3 with strech challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string PlayAgain;
+        do
+        {
+            Random randomGenerator = new Random();
+            int MajicNumber = randomGenerator.Next(1, 11);
+            int Guess;
+            int NumberOfGuesses = 0;
+            do 
+            {
+                NumberOfGuesses++;
+                Console.Write("What is your guess? ");
+                Guess = int.Parse(Console.ReadLine());
+                if (Guess > MajicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if(Guess < MajicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }else{
+                    Console.WriteLine($"You guessed it with only {NumberOfGuesses} guesses");
+                }
+            } while (Guess != MajicNumber);
+            Console.Write("Do you want to play agian? ");
+            PlayAgain = Console.ReadLine();
+        } while(PlayAgain == "yes" || PlayAgain == "Yes");
+
     }
 }

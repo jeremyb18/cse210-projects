b51ed44d5f9435a2aae96b744ee6f041ad22e880
Jeremy Ruebush <jeremyb31415@gmail.com>
2023-01-10 10:57:11 -0700

    Completed Prep 5!

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult(PromptUserName(),PromptUserNumber());
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    static void DisplayResult(string name, int favNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {favNumber*favNumber}");
     }
 }

aec2e2a17b87e861f98e69a5b82d6981ee7cdb66
Jeremy Ruebush <jeremyb31415@gmail.com>
2023-01-12 14:09:38 -0700

    Finished classes to dispay my resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1 = new Job();
+        job1._jobTitle = "Digital Print Machine Operator";
+        job1._company = "HandStands Promo";
+        job1._startYear = 2020;
+        job1._endYear = 2020;
+        //job1.Display();
+        Job job2 = new Job();
+        job2._jobTitle = "Product Design Engineer Intern";
+        job2._company = "EJ Engineering";
+        job2._startYear = 2020;
+        job2._endYear = 2020;
+        //job2.Display();
+        Job job3 = new Job();
+        job3._jobTitle = "Lake Maintenance Specialist";
+        job3._company = "Stansbury Park Services";
+        job3._startYear = 2022;
+        job3._endYear = 2022;
+        //job3.Display();
+        Resume myResume = new Resume();
+        myResume._name = "Jeremy Ruebush";
+        myResume._jobs = new List<Job>{job1,job2,job3};
+        myResume.Display();
+
     }
+}
+
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public int _startYear = 0;
+    public int _endYear = 0;
+
+    public Job()
+    {
+    }
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+}
+
+public class Resume
+{
+    public string _name = "";
+    public List<Job>  _jobs = new List<Job>();
+
+
+    public Resume()
+    {
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Jobs: ");
+        foreach(Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+
 }
